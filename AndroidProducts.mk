796074ee441d032be440ccf4d6e1c3851b175973
Rondeau79 <rondeau7@gmail.com>
2023-08-30 20:03:38 -0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,10 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device.mk \
     $(LOCAL_DIR)/lineage_nio.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_nio-user \
+    lineage_nio-userdebug \
+    lineage_nio-eng 
