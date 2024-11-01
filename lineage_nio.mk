a3e8d6ebacabf1a12c8c8955a7584951fff857e9
Rondeau79 <rondeau7@gmail.com>
2024-10-31 18:28:35 -0600

    Update lineage_nio.mk

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -7,8 +7,8 @@
 # Inherit from nio device
 $(call inherit-product, device/motorola/nio/device.mk)
 
-$ Inherit some common Evolution X stuff
-include device/motorola/nio/device-evolution.mk
+# Inherit some common Evolution X stuff
+$(call inherit-product, device/motorola/nio/device-evolution.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_nio
