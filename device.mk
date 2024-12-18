dc781e1685fb31261c29718f439923d21e064339
Rondeau79 <rondeau7@gmail.com>
2024-12-18 01:41:17 -0700

    Gotta add Viper!

--- device.mk
+++ device.mk
@@ -42,3 +42,8 @@ PRODUCT_COPY_FILES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/motorola/nio/nio-vendor.mk)
+
+#Viper4Android
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+
+
