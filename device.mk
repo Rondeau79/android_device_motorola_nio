a5914a1cac2f74e21c233983d35ae886f4ea7847
David Rondeau <rondeau7@gmail.com>
2026-05-04 22:23:16 -0600

    drop Pixel Parts

--- device.mk
+++ device.mk
@@ -40,9 +40,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp
 
-# PixelParts
-include packages/apps/PixelParts/device.mk  
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
